5120a447984f5072a1084b3bd03bd50b5a14cb57
Tea.rice <teaghuang@gmail.com>
2026-01-17 15:54:25 -0500

    Turret Making (woohoo - tyler)

---
+++ src/main/java/frc/robot/subsystems/turret/Turret.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.turret;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Turret extends SubsystemBase{
+//hi tyler
+//hi tea
+// hi ananya
+//hi sam
+
+  private final TalonFX driveTalon;
+}
+
