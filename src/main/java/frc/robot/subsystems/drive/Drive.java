b4f2bcfb13a2a04316dfdfe287ca99fc77e3aa57
Kabir Kaul <kaulkabir@gmail.com>
2026-02-12 22:49:22 -0500

    added math.abs

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -329,9 +329,9 @@ public class Drive extends SubsystemBase {
 
   /** Returns if the positional vector of the robot is within a setpoint to the bump */
   public boolean closeToBump() {
-    if ((getPose().getTranslation().getY() - (AllianceFlipUtil.apply(FieldConstants.LeftBump.middle).getY())) < 1){
-      if (((getPose().getTranslation().getX() - (AllianceFlipUtil.apply(FieldConstants.LeftBump.middle).getX())) < 1.7) || 
-        (getPose().getTranslation().getX() - (AllianceFlipUtil.apply(FieldConstants.RightBump.middle).getX())) < 1.7){
+    if (Math.abs(getPose().getTranslation().getY() - (AllianceFlipUtil.apply(FieldConstants.LeftBump.middle).getY())) < 1){
+      if ((Math.abs(getPose().getTranslation().getX() - (AllianceFlipUtil.apply(FieldConstants.LeftBump.middle).getX())) < 1.7) || 
+        (Math.abs(getPose().getTranslation().getX() - (AllianceFlipUtil.apply(FieldConstants.RightBump.middle).getX())) < 1.7)){
       return true;
       }
       else return false;
