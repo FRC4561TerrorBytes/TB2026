6a34cd9305a8e7f5afcd310b868f5b86d1975b2c
Kabir Kaul <kaulkabir@gmail.com>
2026-02-28 09:24:34 -0500

    switched x and y on joystick drive at angle command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -270,7 +270,7 @@ public class RobotContainer {
                 .ignoringDisable(true));
     
     driverController.a().onTrue(Commands.runOnce(() -> {snapRotation = drive.snap45();}));
-    driverController.a().whileTrue(DriveCommands.joystickDriveAtAngle(drive, driverController::getLeftX, driverController::getLeftY, () -> snapRotation));
+    driverController.a().whileTrue(DriveCommands.joystickDriveAtAngle(drive, driverController::getLeftY, driverController::getLeftX, () -> snapRotation));
     driverController.rightTrigger().whileTrue(drive.alignToAngle(() -> drive.getRotationToHub()));
 
 
