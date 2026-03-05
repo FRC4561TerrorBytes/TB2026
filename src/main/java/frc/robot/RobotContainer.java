c8eb8d768a5652a0800250e1d5fea0aa5e5b2a31
Tea.rice <teaghuang@gmail.com>
2026-03-05 18:17:39 -0500

    I am dum🍪, atossss!!!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -24,6 +24,7 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -39,6 +40,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.AutoShootCommand;
 import frc.robot.commands.AutoShootCommand;
 import frc.robot.commands.DriveCommands;
 import frc.robot.generated.TunerConstants;
@@ -72,9 +74,12 @@ import frc.robot.subsystems.vision.VisionIOLimelight;
 import frc.robot.util.RobotVisualizer;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
@@ -88,12 +93,12 @@ public class RobotContainer {
   private final Indexer indexer;
   private final Climber climber;
 
-  Rotation2d snapRotation;
-  // Controller
-  private final CommandXboxController driverController = new CommandXboxController(0);
+    Rotation2d snapRotation;
+    // Controller
+    private final CommandXboxController driverController = new CommandXboxController(0);
 
-  // Dashboard inputs
-  private final LoggedDashboardChooser<Command> autoChooser;
+    // Dashboard inputs
+    private final LoggedDashboardChooser<Command> autoChooser;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -121,8 +126,7 @@ public class RobotContainer {
               new ShooterIOReal());
         indexer =
             new Indexer(new IndexerIOReal());
-        climber =
-            new Climber(new ClimberIOReal());
+        climber = new Climber(new ClimberIOReal());
         break;
       case SIM:
         // Sim robot, instantiate physics sim IO implementations
@@ -143,8 +147,6 @@ public class RobotContainer {
               new ShooterIO() {});
         indexer =
             new Indexer(new IndexerIO() {});
-        climber =
-            new Climber(new ClimberIOSim());
         break;
       default:
         // Replayed robot, disable IO implementations
@@ -170,32 +172,47 @@ public class RobotContainer {
         break;
     }
 
-    // Register NamedCommands for use in PathPlanner // TAKE INTAKE COMMAND TIMEOUT OUT (FOR SIM)
-    // Set up auto routines
-    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
-    autoChooser.addOption("Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
-    /*
-    autoChooser.addOption(
-        "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
-    autoChooser.addOption(
-        "Drive SysId (Quasistatic Forward)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Drive SysId (Quasistatic Reverse)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    autoChooser.addOption(
-        "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-        */
-    autoChooser.addOption("Leave and Stop", Commands.run(() -> drive.runVelocity(new ChassisSpeeds(-0.25, 0, 0)), drive).withTimeout(4));
+        // Register NamedCommands for use in PathPlanner // TAKE INTAKE COMMAND TIMEOUT
+        // OUT (FOR SIM)
+        // Set up auto routines
+        autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+        NamedCommands.registerCommand("intake", Commands.run(() -> intake.setOutput(1), intake));
+        NamedCommands.registerCommand("shoot", Commands.sequence(drive.alignToAngle(() -> drive.getRotationToHub()),
+                new AutoShootCommand(drive, indexer, shooter)));
+        // Make into the constant NOT DONE YET DONT RUN AAAAAAAAAAAAAA
+        NamedCommands.registerCommand("slapdown", Commands.runOnce(() -> extension.setExtensionSetpoint(1)));
 
-    SmartDashboard.putData(CommandScheduler.getInstance());
+        // Set up SysId routines
+        autoChooser.addOption(
+                "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
+        /*
+         * autoChooser.addOption(
+         * "Drive Simple FF Characterization",
+         * DriveCommands.feedforwardCharacterization(drive));
+         * autoChooser.addOption(
+         * "Drive SysId (Quasistatic Forward)",2
+         * drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+         * autoChooser.addOption(
+         * "Drive SysId (Quasistatic Reverse)",
+         * drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+         * autoChooser.addOption(
+         * "Drive SysId (Dynamic Forward)",
+         * drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
+         * autoChooser.addOption(
+         * "Drive SysId (Dynamic Reverse)",
+         * drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+         */
+        autoChooser.addOption(
+                "LeaveAndStop",
+                Commands.run(() -> drive.runVelocity(new ChassisSpeeds(-0.25, 0, 0)), drive)
+                        .withTimeout(4));
+
+        SmartDashboard.putData(CommandScheduler.getInstance());
 
     RobotVisualizer.initialize(extension, shooter);
-    // Configure the button bindings
-    configureButtonBindings();
-  }
+        // Configure the button bindings
+        configureButtonBindings();
+    }
 
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
@@ -203,7 +220,6 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  
   private void configureButtonBindings() {
     // Default command, normal field-relative drive
     drive.setDefaultCommand(
@@ -213,21 +229,10 @@ public class RobotContainer {
             () -> -driverController.getLeftX(),
             () -> -driverController.getRightX()));
             
+
+    // Default Commands
     intake.setDefaultCommand(
         Commands.run( () -> intake.setOutput(0), intake));
-
-    indexer.setDefaultCommand(
-      Commands.run(() -> indexer.stop(), indexer));
-
-        // Trigger
-    Trigger bumpPositionTrigger = new Trigger(() -> drive.closeToBump());
-    Trigger intakeExtendedTrigger = new Trigger(() -> extension.extensionSetpoint() == Constants.EXTENSION_EXTENDED_POSITION);
-
-
-    bumpPositionTrigger
-    .and(intakeExtendedTrigger)
-    .and(() -> DriverStation.isTeleop())
-    .whileTrue(driverRumbleCommand());
     indexer.setDefaultCommand(Commands.run(() -> indexer.stop(), indexer));
     shooter.setDefaultCommand(Commands.run(()-> shooter.stop(), shooter));
     // Triggers
