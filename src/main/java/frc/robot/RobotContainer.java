55311ae128ba31bd24841d1129827672fde2635b
Tyler Melton <108908366+SpeedyCoder37@users.noreply.github.com>
2026-03-05 22:26:21 -0500

    Fixed issues (#46)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -209,6 +209,7 @@ public class RobotContainer {
 
         SmartDashboard.putData(CommandScheduler.getInstance());
 
+        RobotVisualizer.initialize(extension, shooter);
         // Configure the button bindings
         configureButtonBindings();
     }
