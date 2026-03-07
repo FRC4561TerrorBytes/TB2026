42b4ad3273b9e4c07d68c8c069eaeda5ac90c06f
programming3Laptop <terrorbytes4561@gmail.com>
2026-03-07 13:09:41 -0500

    Update at 'Sat Mar 07 13:09:41 EST 2026'

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -91,8 +91,8 @@ public class DriveCommands {
           // Convert to field relative speeds & send command
           ChassisSpeeds speeds =
               new ChassisSpeeds(
-                  linearVelocity.getX() * Math.pow(drive.getMaxLinearSpeedMetersPerSec(), 2),
-                  linearVelocity.getY() * Math.pow(drive.getMaxLinearSpeedMetersPerSec(), 2),
+                  Math.pow(linearVelocity.getX() * drive.getMaxLinearSpeedMetersPerSec(), 2) * Math.signum(linearVelocity.getX()),
+                  Math.pow(linearVelocity.getY() * drive.getMaxLinearSpeedMetersPerSec(), 2) * Math.signum(linearVelocity.getY()),
                   omega * drive.getMaxAngularSpeedRadPerSec());
           boolean isFlipped =
               DriverStation.getAlliance().isPresent()
