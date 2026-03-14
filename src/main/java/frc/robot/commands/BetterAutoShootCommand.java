356fbab54d8667b5e63011aa99b40b9baa813936
Tea.rice <teaghuang@gmail.com>
2026-03-14 12:21:08 -0400

    auto shoot command faster

---
+++ src/main/java/frc/robot/commands/BetterAutoShootCommand.java
@@ -0,0 +1,70 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.indexer.Indexer;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.FieldConstants;
+import frc.robot.util.AllianceFlipUtil;
+
+public class BetterAutoShootCommand extends Command {
+
+    public Drive drive;
+    public Indexer indexer;
+    public Shooter shooter;
+    public double distanceToHub;
+
+    public double targetAngle;
+    public double shootSpeedRPS = 70;
+    PIDController controller;
+
+    public BetterAutoShootCommand(Drive drive, Indexer indexer, Shooter shooter) {
+        this.drive = drive;
+        this.indexer = indexer;
+        this.shooter = shooter;
+        addRequirements(drive, indexer, shooter);
+
+        controller = new PIDController(0.075, 0, 0, 0.02);
+        controller.setTolerance(0.5);
+        controller.enableContinuousInput(-180, 180);
+    }
+
+    @Override
+    public void execute() {
+        //distanceToHub = drive.getPose().getTranslation().getDistance(AllianceFlipUtil.apply(FieldConstants.Hub.innerCenterPoint.toTranslation2d()));
+        distanceToHub = drive.getDistanceToHub();
+        shootSpeedRPS = shooter.getFlywheelShootSpeed(distanceToHub);
+        shooter.setFlywheelSpeed(shootSpeedRPS);
+
+        //getting hood angle from the table with interpolation
+        double hoodAngleInterpolated = shooter.interpolateHoodAngle(distanceToHub);
+        shooter.setHoodAngle(hoodAngleInterpolated);
+
+        double targetAngle = drive.getRotationToHub().getDegrees();
+        double rotaionSpeed = MathUtil.clamp(controller.calculate(drive.getPose().getRotation().getDegrees(), targetAngle), -30, 30);
+        drive.runVelocity(new ChassisSpeeds(0, 0, rotaionSpeed));
+
+        if(shooter.leftFlywheelUpToSpeed(shootSpeedRPS) && shooter.rightFlywheelUpToSpeed(shootSpeedRPS) && shooter.hoodAtSetpoint() && controller.atSetpoint()){
+            indexer.setThroughput(0.6, 0.7);
+        }
+        else{
+            indexer.stop();
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shooter.stop();
+        indexer.stop();
+    }
+
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
