0656589e441805d09b642fbd58c04f702439d0fc
programming3Laptop <terrorbytes4561@gmail.com>
2026-03-22 12:33:31 -0400

    Update at 'Sun Mar 22 12:33:31 EDT 2026'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -294,7 +294,11 @@ public class RobotContainer {
 
         driverController
                 .rightTrigger()
-                .whileTrue(new BetterAutoShootCommand(drive, indexer, shooter));
+                .whileTrue(
+                        Commands.parallel(
+                                new BetterAutoShootCommand(drive, indexer, shooter),
+                                agitateBalls()
+                ));
 
 
         driverController
@@ -326,9 +330,9 @@ public class RobotContainer {
                 .povDown()
                 .onTrue(Commands.runOnce(() -> shooter.nudge(-0.1), shooter));
 
-        driverController
-                .povLeft()
-                .whileTrue(agitateBalls());
+        // driverController
+        //         .povLeft()
+        //         .whileTrue(agitateBalls());
 
          driverController
                 .povRight()
@@ -362,9 +366,8 @@ public class RobotContainer {
                         Commands.runOnce(() -> intake.setOutput(0.2), intake).alongWith(driverRumbleCommand())));
         
         operatorController
-        .rightTrigger().whileTrue(
-                new TowerShootCommand(drive, indexer, shooter)
-        );
+                .rightTrigger()
+                .whileTrue(new BetterAutoShootCommand(drive, indexer, shooter));
 
         operatorController
                 .b() // retract intake
@@ -387,10 +390,10 @@ public class RobotContainer {
                 Commands.sequence(
                         Commands.runOnce(()-> intake.setOutput(0.6), intake),
                         Commands.waitSeconds(1),
-                        Commands.runOnce(()-> intake.setOutput(-0.3), intake),
+                        Commands.runOnce(()-> intake.setOutput(-0.2), intake),
                         Commands.waitSeconds(0.2)),
                 Commands.sequence(
-                        Commands.runOnce(()-> extension.setExtensionSetpoint(0.15), extension),
+                        Commands.runOnce(()-> extension.setExtensionSetpoint(0.175), extension),
                         Commands.waitSeconds(1),
                         Commands.runOnce(()-> extension.setExtensionOutput(Constants.EXTENSION_EXTENDED_POSITION), extension))
                 ));
