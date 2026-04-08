a051c6a340b4c74fbbfb58e18652eb58e0f814f5
Tyler Melton <ninjanarwhal07@gmail.com>
2026-04-08 11:16:45 -0400

    increased speed on trench return and increased intake speed on initial swoop

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -169,6 +169,7 @@ public class RobotContainer {
         // Register NamedCommands for use in PathPlanner
         // Set up auto routines
         NamedCommands.registerCommand("intake", Commands.run(() -> intake.setOutput(Constants.INTAKE_SPEED), intake));
+        NamedCommands.registerCommand("intakefast", Commands.run(() -> intake.setOutput(0.7), intake));
         NamedCommands.registerCommand("stopintake", Commands.runOnce(() -> intake.setOutput(0), intake));
         NamedCommands.registerCommand("shoot", RobotCommands.shootWithAgitate(drive, intake, extension, indexer, shooter).withTimeout(5.0));
         NamedCommands.registerCommand("shootpreload", RobotCommands.shootPreload(drive, indexer, shooter).withTimeout(3.5));
